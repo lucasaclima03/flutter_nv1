7ee2bb8c17ffe32f634246db8ba7a4026506cf4b
Lucas Almeida <lucasaclima03@gmail.com>
2022-01-20 09:58:33 -0300

    login page finalizado

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({ Key? key }) : super(key: key);
+  const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(28),
+        child: Column (
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children:  [
+            Icon(Icons.people,
+            size: MediaQuery.of(context).size.height*0.3,
+            ),
+            const TextField(
+              decoration: InputDecoration(
+                label: Text('Login')
+              ),
+            ),
+            const TextField(
+              decoration: InputDecoration(
+                label: Text('Senha'),                
+              ),
+              obscureText: true,
+            ),
+            const SizedBox(height: 15,),
+            ElevatedButton(onPressed: () {} , child: const Text('Login'))
+          ],
+        )
+      )
     );
   }
 }
