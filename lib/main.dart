eb6cbbf2d92ad66e4a21e4471f541291b644fc44
Lucas Almeida <lucasaclima03@gmail.com>
2022-01-20 21:49:11 -0300

    adicionando login controller

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
       initialRoute: '/splash',
       routes: {
         '/splash': (_) => const SplashPage(),
-        '/login': (_) => const LoginPage()
+        '/login': (_) => LoginPage()
       },
     );
   }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,36 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_nv1/controllers/login_controller.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key}) : super(key: key);
+  final LoginController _controller = LoginController();
+
+  LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: const EdgeInsets.all(28),
-        child: Column (
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children:  [
-            Icon(Icons.people,
-            size: MediaQuery.of(context).size.height*0.3,
-            ),
-            const TextField(
-              decoration: InputDecoration(
-                label: Text('Login')
-              ),
-            ),
-            const TextField(
-              decoration: InputDecoration(
-                label: Text('Senha'),                
-              ),
-              obscureText: true,
-            ),
-            const SizedBox(height: 15,),
-            ElevatedButton(onPressed: () {} , child: const Text('Login'))
-          ],
-        )
-      )
-    );
+        body: Container(
+            padding: const EdgeInsets.all(28),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.people,
+                  size: MediaQuery.of(context).size.height * 0.3,
+                ),
+                TextField(
+                  decoration: const InputDecoration(label: Text('Login')),
+                  onChanged: _controller.setLogin,
+                ),
+                TextField(
+                  decoration: const InputDecoration(
+                    label: Text('Senha'),
+                  ),
+                  obscureText: true,
+                  onChanged: _controller.setPass,
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                ValueListenableBuilder<bool>(
+                  valueListenable: _controller.inLoader,
+                  builder: (_, inLoader, __) => inLoader ? const CircularProgressIndicator() : ElevatedButton(
+                    onPressed: () {
+                      _controller.auth().then(
+                        (result) {
+                          if (result) {
+                            print('success');
+                          } else {
+                            print('failed');
+                          }
+                        },
+                      );
+                    },
+                    child: const Text('Login'),
+                  ),
+                ),
+              ],
+            )));
   }
 }
