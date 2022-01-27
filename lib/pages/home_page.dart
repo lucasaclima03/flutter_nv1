54117dc798037990359f28cc4b70f537452159b3
Lucas Almeida <lucasaclima03@gmail.com>
2022-01-27 11:25:17 -0300

    consumindo api e gerenciando estado

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_nv1/controllers/home_controller.dart';
 import 'package:flutter_nv1/models/post_model.dart';
-import 'package:flutter_nv1/repositories/home_repository_mock.dart';
+import 'package:flutter_nv1/repositories/home_repository_imp.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,7 +11,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final HomeController _controller = HomeController(HomeRepositoryMock());
+  final HomeController _controller = HomeController(HomeRepositoryImp());
 
   @override
   void initState() {
@@ -22,16 +22,21 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Center(child: Text('Home')),
+      ),
       body: ValueListenableBuilder<List<PostModel>>(
           valueListenable: _controller.posts,
           builder: (_, list, __) {
-            return ListView.builder(
+            return ListView.separated(
               itemCount: list.length,
               itemBuilder: (_, idx) => ListTile(
                 title: Text(list[idx].title),
               ),
+              separatorBuilder: (_,__) => const Divider() ,
             );
           }),
+          
     );
   }
 }
